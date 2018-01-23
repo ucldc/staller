a2270617175a5f29d4284121a1b283d2e326f6b4
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2018-01-22 19:28:44 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "staller",
-    version = "0.4.3",
+    version = "0.4.5",
     packages = find_packages(),
     install_requires = ['lxml', 'argparse'],
     # metadata for upload to PyPI
